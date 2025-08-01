3330036f45e919e8c369a3ed0fc7e3bfe78af28e
Dawson <dawsonpowell07@gmail.com>
2025-08-01 13:52:17 -0400

    updated agents

--- nodes/summarizer.py
+++ nodes/summarizer.py
@@ -1,1 +1,15 @@
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from tools.get_emails import get_emails
+from tools.gmail_authenticate import gmail_authenticate
+from prompts.summarizer_prompt import summarizer_prompt
+load_dotenv()
 
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+summarizer_agent = create_react_agent(
+    model=llm,
+    tools=[get_emails, gmail_authenticate],
+    prompt=summarizer_prompt,
+)

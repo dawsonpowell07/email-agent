5e9f763a943f7ad5325daeb9e8f65217fb1c2bbe
Dawson <dawsonpowell07@gmail.com>
2025-08-01 14:44:10 -0400

    added auth tool

--- nodes/classifier.py
+++ nodes/classifier.py
@@ -2,6 +2,7 @@ from langgraph.prebuilt import create_react_agent
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from tools.add_to_label import add_to_label
+from tools.gmail_authenticate import gmail_authenticate
 from prompts.classifier_prompt import classifier_prompt
 load_dotenv()
 
@@ -9,6 +10,6 @@ llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
 classifier_agent = create_react_agent(
     model=llm,
-    tools=[add_to_label],
+    tools=[add_to_label, gmail_authenticate],
     prompt=classifier_prompt,
 )
